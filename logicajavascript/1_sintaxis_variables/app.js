13e07fa7cb956d777b4f98359ea12b7aed319101
EimyCaicedo03 <eimycaicedo23@gmail.com>
2025-07-31 12:26:09 -0500

    repositorio js versión 1

---
+++ logicajavascript/1_sintaxis_variables/app.js
@@ -0,0 +1,26 @@
+// el alert muestra una ventana emergente en el navegador 
+alert ("hola mundo")
+//promt muestra una caja para ingresar informacion desde el navegador 
+prompt("dime tu nombre")
+
+//variable declarada con let 
+let personaje = "perrito"
+//crear variable puntaje y asignar valor 0
+// los valore numericos van sin comillas
+let puntaje = 0
+// crear variable shots y asignar valor de 3
+let shots = 3
+    recarga = 2 
+
+console. log("total balas", shots + recarga)
+
+// El usuario capturó 4 patos 
+puntaje += 4
+//El usuario gasto 2 balas 
+shots -= 2 
+// El usuario recargó su arma 
+recarga += 1
+
+console.log("puntaje", puntaje)
+console.log("shots", shots)
+console.log("recarga", recarga)
---
+++ logicajavascript/2_operadores_comparación_logicos/app.js
@@ -0,0 +1,31 @@
+//Bolean(...)
+
+console.log( ' "5" == "5" ' , "5 "== "5" )//true
+console.log( ' "5" === "5" ' , "5" === "5" )//true
+console.log( ' 5 === "5" ' , 5 === "5" )//false
+console.log( ' 5 == "5" ' , 5 == "5" )//true
+console.log( ' 5 === (3+2) ' , "5" === (3+2) )//false
+console.log( ' 5 === 5 ' , 5 === 5 )//true
+
+
+//concatenar (+)
+
+console.log( 5 + "3" ) // "53"
+console.log( 5 + Number("3") ) // "8"
+console.log( 5 * "3" ) // "15"
+console.log( 5 + 3) // 8
+
+//pida dos numeros al usuario y sumelos
+let num1 = Number ( prompt ('dame num 1') )// "5"
+let num2 = Number (prompt ('dame num 2') ) // "3"
+let resultado = num1 + num2
+alert( "El resultado de tu suma es " + resultado )
+
+// pida dos numeros al usiario y sumelos 
+let num3 = Number ( prompt('dame num3' ) )
+let num4 = Number ( prompt ( 'dame num4' ) ) 
+
+// mostrar los valores ingresados
+console.log ( "El valor A es " + num3 + " y el valor B es " + num4 + )
+
+Realiza
